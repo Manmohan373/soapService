5f70b1d72b016895c0f8e3110e5d3bca4fc10022
Laxmidhara-Sahoo-512 <sahoorabindra740@gmail.com>
2024-12-27 15:35:08 +0530

    soap2 change

--- src/main/java/com/alfaris/ipsh/soap/endpoint/EndPoint.java
+++ src/main/java/com/alfaris/ipsh/soap/endpoint/EndPoint.java
@@ -65,7 +65,7 @@ public class EndPoint {
 	@ResponsePayload
 	public  SearchEmployeeResponse search(@RequestPayload SearchEmployeeRequest request) throws RecordNotFoundException {
 		System.out.println("Manmohan");
-				System.out.println("laxmidhara");
+				System.out.println("laxmidhara sahoo" );
 		return service.search(request);
 }
 
